206cd1fdc286f5a8fb92b7c84c227843fe167aae
Krishna Awasthi <krishhawasthi28@gmail.com>
2026-01-20 14:53:03 +0530

    Arrays- 1 uploaded

---
+++ Arrays/SetMatrixZeroes.java
@@ -0,0 +1,50 @@
+// 73. Set Matrix Zeroes
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+// You must do it in place.
+
+class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        boolean firstRowZero=false;
+        boolean firstColZero=false;
+        for(int i=0;i<m;i++){
+            if(matrix[i][0]==0){
+                firstColZero=true;
+                break;
+            }
+        }
+        for(int j=0;j<n;j++){
+            if(matrix[0][j]==0){
+                firstRowZero=true;
+                break;
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
+            }
+        }
+
+        if(firstRowZero){
+            for(int j=0;j<n;j++){
+                matrix[0][j]=0;
+            }
+        }
+        if(firstColZero){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
+            }
+        }
+    }
+}
