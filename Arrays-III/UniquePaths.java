3f1dc81289350f128f62b083554f0d0e473e003f
Krishna Awasthi <krishhawasthi28@gmail.com>
2026-01-22 16:14:22 +0530

    ArraysIII- 5 uploaded

---
+++ Arrays-III/UniquePaths.java
@@ -0,0 +1,16 @@
+//62. Unique Paths
+// Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
+
+class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m+1][n+1];
+        for(int i=0;i<m+1;i++){
+            for(int j=0;j<n+1;j++){
+                if(i==0 || j==0) dp[i][j]=0;
+                else if(i==1 && j==1) dp[i][j]=1;
+                else dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m][n];
+    }
+}
