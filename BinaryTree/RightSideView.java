d5a6ba45923a3e2142554c105331e323791e0ecf
Krishna Awasthi <krishhawasthi28@gmail.com>
2026-01-27 20:12:51 +0530

    BinaryTree- 4,5 uploaded

---
+++ BinaryTree/RightSideView.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class RightSideView {
+    public class TreeNode{
+        int data;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public ArrayList<Integer> rightSideView(TreeNode root){
+        ArrayList<Integer> res=new ArrayList<>();
+        Queue<TreeNode> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                TreeNode node=q.poll();
+                if(node.left!=null) q.offer(node.left);
+                if(node.right!=null) q.offer(node.right);
+                if(i==size-1) res.add(node.data);
+            }
+        }
+        return res;
+    }
+}
