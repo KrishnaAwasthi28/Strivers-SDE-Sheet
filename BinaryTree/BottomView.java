e0f8c31a12ff1038244fc4a1be1e1481ac913a3c
Krishna Awasthi <krishhawasthi28@gmail.com>
2026-01-27 20:55:17 +0530

    BinaryTree- 5,6 uploaded

---
+++ BinaryTree/BottomView.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+public class BottomView {
+    public class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    public class CustomNode{
+        Node node;
+        int col;
+        CustomNode(Node node,int col){
+            this.node=node;
+            this.col=col;
+        }
+    }
+    public ArrayList<Integer> bottomView(Node root){
+        ArrayList<Integer> res=new ArrayList<>();
+        Queue<CustomNode> q=new LinkedList<>();
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        q.offer(new CustomNode(root, 0));
+        while(!q.isEmpty()){
+            CustomNode custom=q.poll();
+            Node node=custom.node;
+            int col=custom.col;
+            map.put(col,node.data);
+            if(node.left!=null) q.offer(new CustomNode(node.left, col-1));
+            if(node.right!=null) q.offer(new CustomNode(node.right, col+1));
+        }
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            res.add(entry.getValue());
+        }
+        return res;
+    }
+}
