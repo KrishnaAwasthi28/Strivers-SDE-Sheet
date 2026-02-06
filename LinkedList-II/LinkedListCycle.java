ba7f10b8291205601afc08d4a734507468036cf6
Krishna Awasthi <krishhawasthi28@gmail.com>
2026-02-06 20:12:57 +0530

    LinkedList-II 2 uploaded

---
+++ LinkedList-II/LinkedListCycle.java
@@ -0,0 +1,21 @@
+public class LinkedListCycle {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast) return true;
+        }
+        return false;
+    }
+}
