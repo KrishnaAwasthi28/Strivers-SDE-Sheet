8f368b96afd7c49fd761f0f9f0417794121b70d0
Krishna Awasthi <krishhawasthi28@gmail.com>
2026-02-06 21:35:58 +0530

    LinkedList-II 3 uploaded

---
+++ LinkedList-II/ReverseNodesInKGroup.java
@@ -0,0 +1,44 @@
+public class ReverseNodesInKGroup {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public int findLen(ListNode head){
+        int counter=0;
+        while(head!=null){
+            counter++;
+            head=head.next;
+        }
+        return counter;
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        int n = findLen(head);
+        int groups = n / k;
+        ListNode curHead = head;
+        ListNode cur = head;
+        ListNode prevHead = null;
+        for (int g = 0; g < groups; g++) {
+            ListNode prev = null;
+            for (int i = 0; i < k; i++) {
+                ListNode nextNode = cur.next;
+                cur.next = prev;
+                prev = cur;
+                cur = nextNode;
+            }
+            if (prevHead == null) {
+                head = prev;
+            } else {
+                prevHead.next = prev;
+            }
+            prevHead = curHead;
+            curHead = cur;
+        }
+        prevHead.next = curHead; // last group
+        return head;
+    }
+}
