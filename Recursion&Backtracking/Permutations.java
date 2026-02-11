6a51a7cb55fffa94a6c8c9de2e1d2404b52f61ff
Krishna Awasthi <krishhawasthi28@gmail.com>
2026-02-11 21:28:39 +0530

    Backtracking- 1 uploaded

---
+++ Recursion&Backtracking/Permutations.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Permutations {
+    public void helper(int[] nums,int idx,List<List<Integer>> ans){
+        if(idx==nums.length-1){
+            List<Integer> l=new ArrayList<>();
+            for(int i=0;i<nums.length;i++){
+                l.add(nums[i]);
+            }
+            ans.add(l);
+            return;
+        }
+        for(int i=idx;i<nums.length;i++){
+            swap(i,idx,nums);
+            helper(nums,idx+1,ans);
+            swap(i,idx,nums);
+        }
+    }
+    public void swap(int i,int j,int[] nums){
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(nums,0,ans);
+        return ans;
+    }
+}
